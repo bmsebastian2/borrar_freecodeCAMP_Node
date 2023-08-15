42b3e382df46a940668ca8beb9db264ad97bef24
Replit user <>
2023-08-15 17:30:02 +0000

    Servir activos estaticos 3

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ let app = express();
 let path = require('path')
 let PORT =8080
 let pathFile = path.join(__dirname + '/views/index.html')
-
+let pathStatic =path.join(__dirname + '/public')
 console.log(pathFile)
 // 1 # Conoce la Console
 console.log('Hello World')
@@ -12,7 +12,7 @@ console.log('Hello World')
 // 2 # Servidor expreso de trabajo
 // 3 # Servir un archivo HTML
 // 4 # Servir activos estáticos
-app.use(express.static(path.join(__dirname + '/public')))
+app.use(express.static(pathStatic))
 
 app.get('/', (req, res) => {
   res.sendFile(pathFile)
