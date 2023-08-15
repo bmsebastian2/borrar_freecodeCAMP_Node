c198040f1060cae8b10f52a4c4bd49caacf62439
Replit user <>
2023-08-15 15:08:56 +0000

    Servir un archivo HTML

--- myApp.js
+++ myApp.js
@@ -1,30 +1,24 @@
 let express = require('express');
 let app = express();
+let path = require('node:path')
 let PORT =8080
+let pathFile = path.join(__dirname + '/views/index.html')
 
+console.log(pathFile)
 // 1 # Conoce la Console
 console.log('Hello World')
 
 
 // 2 # servidor expreso de trabajo
+// 3 # Servir un archivo HTML
 app.get('/', (req, res) => {
-  res.send('Hello Express')
+  res.sendFile(pathFile)
 })
 
-// 3 #
+
+
 
 app.listen(PORT, () => console.log('TODO BIEN:'+PORT))
-
-
-
-
-
-
-
-
-
-
-
 
 
 
