828c36a69a8630963311bf9a8a9198c62769af3a
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2023-08-17 08:43:36 -0300

    7 mudlaware a nivel raiz

--- myApp.js
+++ myApp.js
@@ -1,53 +1,32 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-let path = require('path')
-let PORT =8080
-let pathFile = path.join(__dirname + '/views/index.html')
-let pathStatic =path.join(__dirname + '/public')
-console.log(pathFile)
+let path = require("path");
+let PORT = 8080;
+let pathFile = path.join(__dirname + "/views/index.html");
+let pathStatic = path.join(__dirname + "/public");
+// 7 # middleware nivel raiz
+app.use((req, res, next) => {
+  const { path, ip, method } = req;
+  console.log(`${method} ${path} - ${ip}`);
+  next();
+});
 // 1 # Conoce la Console
-console.log('Hello World')
-
+console.log("Hello World");
 
 // 2 # Servidor expreso de trabajo
 // 3 # Servir un archivo HTML
 // 4 # Servir activos estáticos
-app.use(express.static(pathStatic))
+app.use(express.static(pathStatic));
 
-app.get('/', (req, res) => {
-  res.sendFile(pathFile)
-})
+app.get("/", (req, res) => {
+  res.sendFile(pathFile);
+});
 
 // 5 # Servir JSON
-app.get('/json',(req,res)=>{
-  res.send({"message": "Hello json"})
-})
+app.get("/json", (req, res) => {
+  res.send({ message: "Hello json" });
+});
 
-
-
-
-app.listen(PORT, () => console.log('TODO BIEN:'+PORT))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.listen(PORT, () => console.log("TODO BIEN:" + PORT));
 
 module.exports = app;
